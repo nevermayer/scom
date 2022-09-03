4f519ea89293c2a7b263438a54c06c41cf0f6b17
asamueldx <salvarezp@fcpn.edu.bo>
2022-09-03 19:16:25 -0400

    tabla facturq

--- database/migrations/2022_08_30_022029_create_factura_table.php
+++ database/migrations/2022_08_30_022029_create_factura_table.php
@@ -9,12 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      *
-     * @return void
+     * @return void  
      */
     public function up()
     {
         Schema::create('factura', function (Blueprint $table) {
             $table->id();
+            $table->double('total');
+            $table->date('fecha');
+            $table->foreignId('cajero_id')
+            ->constrained('cajero');
+            $table->foreignId('orden_id')
+            ->constrained('orden');
+            $table->foreignId('cliente_id')
+            ->constrained('cliente');
             $table->timestamps();
         });
     }
