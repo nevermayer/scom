b7ff2793f67db64dc2f5d558af19262fec84b09b
asamueldx <salvarezp@fcpn.edu.bo>
2022-09-03 19:16:37 -0400

    tabla mesa

--- database/migrations/2022_08_30_022039_create_mesa_table.php
+++ database/migrations/2022_08_30_022039_create_mesa_table.php
@@ -9,12 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      *
-     * @return void
+     * @return void  
+
      */
     public function up()
     {
         Schema::create('mesa', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('min_capacidad');
+            $table->string('max_capacidad');
+            $table->string('status');
+            $table->foreignId('orden_id')
+            ->constrained('orden');
+            $table->foreignId('camarero_id')
+            ->constrained('camarero');
+            $table->foreignId('cliente_id')
+            ->constrained('cliente');
             $table->timestamps();
         });
     }
