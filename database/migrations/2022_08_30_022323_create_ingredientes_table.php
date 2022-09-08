5384240729b76f06a19dabddcaace1f1b839c864
danisz23 <112452902+danisz23@users.noreply.github.com>
2022-09-07 21:52:53 -0400

    prueba

--- database/migrations/2022_08_30_022323_create_ingredientes_table.php
+++ database/migrations/2022_08_30_022323_create_ingredientes_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('ingredientes', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('stock');
+            $table->string('nombre');
+            $table->timestamps('fecha_vencimiento');
         });
     }
 
