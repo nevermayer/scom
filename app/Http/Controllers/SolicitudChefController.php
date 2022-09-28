fd15922666ea62273ed047a0e010f4a4a9bbe423
henrrycasa <githycc@gmail.com>
2022-09-28 20:02:51 -0300

    Controlador_Solicitud

--- app/Http/Controllers/SolicitudChefController.php
+++ app/Http/Controllers/SolicitudChefController.php
@@ -9,29 +9,27 @@ use Illuminate\Http\Request;
 
 class SolicitudChefController extends Controller
 {
-    
+
     public function Solicitud(Request $request)
     {
         $data = json_decode($request->items, true);
-        $all_ids = array();
-        foreach ($data as $el) {
-            array_push($all_ids, $el['id']);
-        }
-        $chef = Chef::create([
-            'id_usuario' => $request->id_usuario
-        ]);
-        foreach($data as $res){
-            $chef->ingredientes()->attach($res['id'],[
-                'cantidad'=>$res['qty'],
-                'status'=>$res['qty'],
-                'fecha'=>$res['qty']
+
+        $ingrediente = Ingrediente::findOrFail($request);
+
+        $chef = Chef::findOrFail($request);
+
+        foreach ($data as $res) {
+
+            $chef->ingredientes()->attach($res['id'], [
+                'cantidad' => $res['qty'],
+                'status' => $res['status'],
+                'fecha' => date('j/n/Y')
             ]);
-            Ingrediente::find($res['id'])->decrement('cantidad',$res['qty']);
+            $chef->save();
         }
-        $msg="se creo la solicitud";
+        $msg = "se creo la solicitud";
         return response()->json([
             "message" => $msg
         ]);
     }
-
 }
