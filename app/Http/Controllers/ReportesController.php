3cc66be957839cd648a475c1550e3b844326a3a0
henrrycasa <githycc@gmail.com>
2022-10-11 23:13:01 -0300

    Informes de Ingresos y egresos mensuales

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -10,7 +10,8 @@ class ReportesController extends Controller
     public function index(Request $request){
         $fecha = date('Y-m-d');
         $factura = Factura::where('fecha','=', $fecha)->where('fecha','=', $fecha)->sum('total');
-        $semana=500;
+        
+        $semana="SELECT SUM(total) as 'total' FROM $factura WHERE fecha BETWEEN $fecha" ;
         return response()->json([
             "hoy" => $factura,
         "semana"=>$semana]);
