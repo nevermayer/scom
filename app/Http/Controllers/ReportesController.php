2e1921d8c05154f52139f4195874076105d95c3b
nevermayer <jlrspower@gmail.com>
2022-10-13 23:31:15 -0400

    reportes

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -9,9 +9,11 @@ class ReportesController extends Controller
 {
     public function index(Request $request){
         $fecha = date('Y-m-d');
+        $from = date('2018-01-01');
+        $to = date('Y-m-d');
         $factura = Factura::where('fecha','=', $fecha)->where('fecha','=', $fecha)->sum('total');
-        
-        $semana="SELECT SUM(total) as 'total' FROM $factura WHERE fecha BETWEEN $fecha" ;
+        $semana=Factura::whereBetween('fecha',[$from, $to])->sum('total');
+       // $semana="SELECT SUM(total) as 'total' FROM $factura WHERE fecha BETWEEN $fecha" ;
         return response()->json([
             "hoy" => $factura,
         "semana"=>$semana]);
